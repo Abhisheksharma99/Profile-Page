a584dad1e4f2872676cad280e2df5197be77b043
Abhishek Sharma <61747336+Abhisheksharma99@users.noreply.github.com>
2021-01-24 17:01:50 +0530

    Add files via upload

---
+++ abhisheksharma999r-ME_QPROFILE/profile-frontend/src/data/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // TODO: change your profile information here
+  name: "Abhishek Sharma",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Abhishek Sharma and I'm a Software Engineer!",
+  githubUrl: "https://github.com/Abhisheksharma99",
+  linkedinUrl: "https://www.linkedin.com/in/abhishek-sharma-663b08197/",
+  cvLink: "https://drive.google.com/file/d/1PW2Dkpn7AOWac3dal84rzQQuyu1YSFd9/view?usp=sharing",
+};
